c1b2175cb2d68193b86bdac57520841093abdb22
hannahbananabro <hannah5131@icloud.com>
2026-01-20 13:08:38 -0600

    Create webp_to_gif.py

---
+++ src/webp_to_gif.py
@@ -0,0 +1,22 @@
+import os
+
+from PIL import Image
+
+
+def webp_to_gif(filepath: str) -> None:
+    gif = Image.open(filepath)
+    if gif.format == 'WEBP':
+        gif.info.pop('background', None)
+        gif.save(filepath, 'gif', save_all=True, lossless=True, quality=100, method=6, exact=True)
+
+
+# https://stackoverflow.com/questions/52016407/how-to-convert-webp-image-to-gif-with-python
+def convert_all_webp_to_gif() -> None:
+    for (dirpath, dirnames, filenames) in os.walk('assets'):
+        for filename in filenames:
+            try:
+                if filename.endswith('.gif'):
+                    filepath = os.path.join(dirpath, filename)
+                    webp_to_gif(filepath)
+            except:
+                continue
