80e0fadbfd3dfba00941d0569609360de58a29e0
hannahbananabro <hannah5131@icloud.com>
2026-01-20 11:22:29 -0600

    Refactor Twitter API authorization and tweeting logic

--- main.py
+++ main.py
@@ -15,42 +15,42 @@ def get_random_media():
 
 # Authorize Twitter with v1.1 API
 def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
-...     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-...     auth.set_access_token(access_token, access_token_secret)
-...     return tweepy.API(auth)
-... 
-... 
-... # Authorize Twitter with v2 API
-... def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
-...     return tweepy.Client(
-...         consumer_key=consumer_key, consumer_secret=consumer_secret,
-...         access_token=access_token, access_token_secret=access_token_secret,
-...         return_type=requests.Response,
-...     )
-... 
-... 
-... # Tweet picked image/video
-... def tweet(media) -> requests.Response:
-...     # In the next for 4 lines we are getting the keys from Step 4
-...     consumer_key = os.environ['CONSUMER_KEY']
-...     consumer_secret = os.environ['CONSUMER_SECRET']
-...     access_token = os.environ['ACCESS_TOKEN']
-...     access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-... 
-...     api_v1 = auth_v1(consumer_key, consumer_secret,
-...                      access_token, access_token_secret)
-...     client_v2 = auth_v2(consumer_key, consumer_secret,
-...                         access_token, access_token_secret)
-... 
-...     media_id = api_v1.media_upload(media).media_id
-... 
-...     return client_v2.create_tweet(media_ids=[media_id])
-... 
-... 
-... def main():
-...     medias = get_random_medias()
-...     tweet(medias)
-... 
-... 
-... if __name__ == '__main__':
-...     main()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth)
+
+
+# Authorize Twitter with v2 API
+def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
+    return tweepy.Client(
+        consumer_key=consumer_key, consumer_secret=consumer_secret,
+        access_token=access_token, access_token_secret=access_token_secret,
+        return_type=requests.Response,
+    )
+
+
+# Tweet picked image/video
+def tweet(media) -> requests.Response:
+    # In the next for 4 lines we are getting the keys from Step 4
+    consumer_key = os.environ['CONSUMER_KEY']
+    consumer_secret = os.environ['CONSUMER_SECRET']
+    access_token = os.environ['ACCESS_TOKEN']
+    access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+    api_v1 = auth_v1(consumer_key, consumer_secret,
+                     access_token, access_token_secret)
+    client_v2 = auth_v2(consumer_key, consumer_secret,
+                        access_token, access_token_secret)
+
+    media_id = api_v1.media_upload(media).media_id
+
+    return client_v2.create_tweet(media_ids=[media_id])
+
+
+def main():
+    medias = get_random_media()
+    tweet(medias)
+
+
+if __name__ == '__main__':
+    main()
